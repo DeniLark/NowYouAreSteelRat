58d6b49e2cb3a7c876317a2412f76469e02b5f27
Deni Lark <denlarsites@gmail.com>
2023-04-26 13:58:54 +0500

    check if all chapters exist

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import           Data.Foldable
+import           Data.Function
+import           Data.List                      ( sortBy )
+
+import           Book.IO
+import           Book.Types.Chapter
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = checkAllChaptersForExistence
+
+checkAllChaptersForExistence :: IO ()
+checkAllChaptersForExistence = do
+  chapters <- sortBy (compare `on` chapterId) <$> getAllChapters
+  _        <- foldlM checkOneChapter 0 chapters
+  pure ()
+ where
+  checkOneChapter :: Int -> Chapter -> IO Int
+  checkOneChapter n ch
+    | n == chapterId ch
+    = pure $ n + 1
+    | otherwise
+    = putStrLn ("Chapter " <> show n <> " is not exist")
+      >> checkOneChapter (n + 1) ch
