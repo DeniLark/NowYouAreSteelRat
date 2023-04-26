3cb23f7fd1672d217388f4addf719210527a867a
Deni Lark <denlarsites@gmail.com>
2023-04-26 12:36:04 +0500

    getting all the chapters

--- src/Book/IO.hs
+++ src/Book/IO.hs
@@ -3,11 +3,17 @@ module Book.IO where
 
 import           Data.Aeson
 import qualified Data.ByteString               as B
+import           System.Directory
 
 import           Book.Types.Chapter
 
 pathBook :: FilePath
 pathBook = "./book/chapters/"
+
+getAllChapter :: IO [Chapter]
+getAllChapter = do
+  filePaths <- listDirectory pathBook
+  foldMap getChaptersFromFile filePaths
 
 getChaptersFromFile :: FilePath -> IO [Chapter]
 getChaptersFromFile filePath = do
